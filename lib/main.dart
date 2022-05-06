8ee24b56e02f2279a4aed2aace8a954b264285e1
SHIUAN3543\ekek3 <ekek3333@gmail.com>
2022-05-07 01:11:17 +0800

    change to chinese but garbled text

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:chitchat_internship/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: '面試題目'),
+    );
+  }
+}
